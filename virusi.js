35945457b461ee295e1b22b9d4b2bc39b07e83ce
Gifted Devs <166598671+mouricedevs@users.noreply.github.com>
2024-05-26 07:54:13 +0300

    𝗩𝗜𝗥𝗨𝗦𝗜-𝗠𝗕𝗔𝗬𝗔-𝗠𝗗

--- virusi.js
+++ virusi.js
@@ -1,15 +1,18 @@
+
 const express = require("express");
 const app = express();
 
+
+
+
+
 const pino = require("pino");
 let { toBuffer } = require("qrcode");
-const path = require("path");
+const path = require('path');
 const fs = require("fs-extra");
 const { Boom } = require("@hapi/boom");
-const PORT = process.env.PORT || 5000;
-const MESSAGE =
-  process.env.MESSAGE ||
-  `
+const PORT = process.env.PORT ||  5000
+const MESSAGE = process.env.MESSAGE ||  `
 *✅sᴇssɪᴏɴ ᴄᴏɴɴᴇᴄᴛᴇᴅ✅*
 *Made With 💜*
 ______________________________
@@ -29,100 +32,122 @@ ______________________________
 ______________________________
 
 Don't Forget To Give Star⭐ To My Repo
-`;
+`
 
-if (fs.existsSync("/kish_baileys")) {
-  fs.emptyDirSync(__dirname + "/kish_baileys");
-}
 
-app.use("", async (req, res) => {
-  const {
-    default: KishWASocket,
-    useMultiFileAuthState,
-    Browsers,
-    delay,
-    DisconnectReason,
-    makeInMemoryStore,
-  } = require("@whiskeysockets/baileys");
-  const store = makeInMemoryStore({
-    logger: pino().child({ level: "silent", stream: "store" }),
-  });
-  async function KISH() {
-    const { state, saveCreds } = await useMultiFileAuthState(
-      __dirname + "/kish_baileys",
-    );
+
+
+
+
+
+
+
+
+
+if (fs.existsSync('./auth_info_baileys')) {
+    fs.emptyDirSync(__dirname + '/auth_info_baileys');
+  };
+  
+  app.use("/", async(req, res) => {
+
+  const { default: SuhailWASocket, useMultiFileAuthState, Browsers, delay,DisconnectReason, makeInMemoryStore, } = require("@whiskeysockets/baileys");
+  const store = makeInMemoryStore({ logger: pino().child({ level: 'silent', stream: 'store' }) })
+  async function SUHAIL() {
+    const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys')
     try {
-      let Smd = KishWASocket({
+      let Smd =SuhailWASocket({ 
         printQRInTerminal: false,
-        logger: pino({ level: "silent" }),
-        browser: ["Virusi", "KishQrScan", ""],
-        auth: state,
-      });
+        logger: pino({ level: "silent" }), 
+        browser: Browsers.baileys("Desktop"),
+        auth: state 
+        });
+
 
       Smd.ev.on("connection.update", async (s) => {
         const { connection, lastDisconnect, qr } = s;
-        if (qr) {
-          res.end(await toBuffer(qr));
-        }
+        if (qr) { res.end(await toBuffer(qr)); }
 
-        if (connection == "open") {
+
+        if (connection == "open"){
           await delay(3000);
           let user = Smd.user.id;
-          let CREDS = fs.readFileSync(
-            __dirname + "/kish_baileys/creds.json",
-          );
-          var Scan_Id = Buffer.from(CREDS).toString("base64");
-          // res.json({status:true,Scan_Id })
+
+
+//===========================================================================================
+//===============================  SESSION ID    ===========================================
+//===========================================================================================
+
+          let CREDS = fs.readFileSync(__dirname + '/auth_info_baileys/creds.json')
+          var Scan_Id = Buffer.from(CREDS).toString('base64')
+         // res.json({status:true,Scan_Id })
           console.log(`
 ====================  SESSION ID  ==========================                   
 SESSION-ID ==> ${Scan_Id}
 -------------------   SESSION CLOSED   -----------------------
-`);
+`)
 
-          let msgsss = await Smd.sendMessage(user, {
-            text: `Virusi;;;${Scan_Id}`,
+
+          let msgsss = await Smd.sendMessage(user, { 
+          text: `Virusi;;;${Scan_Id}` 
           });
-          await Smd.sendMessage(user, { text: MESSAGE }, { quoted: msgsss });
+          await Smd.sendMessage(user, { text: MESSAGE } , { quoted : msgsss });
           await delay(1000);
-          try {
-            await fs.emptyDirSync(__dirname + "/kish_baileys");
-          } catch (e) {}
+          try{ await fs.emptyDirSync(__dirname+'/auth_info_baileys'); }catch(e){}
+
+
         }
 
-        Smd.ev.on("creds.update", saveCreds);
+        Smd.ev.on('creds.update', saveCreds)
 
-        if (connection === "close") {
-          let reason = new Boom(lastDisconnect?.error)?.output.statusCode;
-          // console.log("Reason : ",DisconnectReason[reason])
-          if (reason === DisconnectReason.connectionClosed) {
-            console.log("Connection closed!");
-            // KISH().catch(err => console.log(err));
-          } else if (reason === DisconnectReason.connectionLost) {
-            console.log("Connection Lost from Server!");
-            //  KISH().catch(err => console.log(err));
-          } else if (reason === DisconnectReason.restartRequired) {
-            console.log("Restart Required, Restarting...");
-            KISH().catch((err) => console.log(err));
-          } else if (reason === DisconnectReason.timedOut) {
-            console.log("Connection TimedOut!");
-            // KISH().catch(err => console.log(err));
-          } 
-        }
+        if (connection === "close") {            
+            let reason = new Boom(lastDisconnect?.error)?.output.statusCode
+            // console.log("Reason : ",DisconnectReason[reason])
+            if (reason === DisconnectReason.connectionClosed) {
+              console.log("Connection closed!")
+             // SUHAIL().catch(err => console.log(err));
+            } else if (reason === DisconnectReason.connectionLost) {
+                console.log("Connection Lost from Server!")
+            //  SUHAIL().catch(err => console.log(err));
+            } else if (reason === DisconnectReason.restartRequired) {
+                console.log("Restart Required, Restarting...")
+              SUHAIL().catch(err => console.log(err));
+            } else if (reason === DisconnectReason.timedOut) {
+                console.log("Connection TimedOut!")
+             // SUHAIL().catch(err => console.log(err));
+            }  else {
+                console.log('Connection closed with bot. Please run again.');
+                console.log(reason)
+              //process.exit(0)
+            }
+          }
+
+
+
       });
     } catch (err) {
-      console.log(err);
-      await fs.emptyDirSync(__dirname + "/kish_baileys");
+        console.log(err);
+       await fs.emptyDirSync(__dirname+'/auth_info_baileys'); 
     }
   }
 
-  KISH().catch(async (err) => {
-    console.log(err);
-    await fs.emptyDirSync(__dirname + "/kish_baileys");
 
-    //// MADE BY KISH INC - 2024
-  });
+
+
+
+
+
+
+  SUHAIL().catch(async(err) => {
+    console.log(err)
+    await fs.emptyDirSync(__dirname+'/auth_info_baileys'); 
+
+
+    //// MADE BY GIFTED
+
 });
 
-app.listen(PORT, () =>
-  console.log(`Virusi-Qr Server Running on Port http://localhost:${PORT}`),
-);
+
+  })
+
+
+app.listen(PORT, () => console.log(`App listened on port http://localhost:${PORT}`));
